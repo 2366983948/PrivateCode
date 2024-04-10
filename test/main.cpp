ddec9320168a1ab570ee72631bd7529e285e1bcd
MoXu <2366983948@qq.com>
2024-04-10 16:48:47 +0800

    优化红黑树

--- test/main.cpp
+++ test/main.cpp
@@ -1,9 +1,15 @@
 #include<bits/stdc++.h>
+#include "../Code/utf-8/RBTRee/RBTree.hpp"
 using namespace std;
 
 int main(){
-    vector<int> v;
-    v.emplace_back(5);
-    cout<<v[0]<<endl;
+    RBTree<int> tree;
+    tree.Insert(1);
+    tree.Insert(5);
+    tree.Insert(0);
+    tree.Insert(3);
+    tree.Insert(-1);
+    tree.Display(); 
+    tree.DisplayData(); 
     return 0;
 }
